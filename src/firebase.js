920b7733944a5086028e1764cb1be29029d0927a
chriscrittendenbyu <50722138+chriscrittendenbyu@users.noreply.github.com>
2019-05-17 17:08:21 -0600

    Update firebase details

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyD76g3n5X4Ll2GVaEb5Ds2vByHymWO8e88",
+  authDomain: "authentication-d0870.firebaseapp.com",
+  databaseURL: "https://authentication-d0870.firebaseio.com",
+  projectId: "authentication-d0870",
+  storageBucket: "authentication-d0870.appspot.com",
+  messagingSenderId: "1071599035383",
+  appId: "1:1071599035383:web:c31746fac99ec883"
 };
 
 firebase.initializeApp(config);
