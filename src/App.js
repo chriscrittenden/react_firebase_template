c17b0ed798e159c1d63e68ce461ef862ee0ae867
chriscrittendenbyu <50722138+chriscrittendenbyu@users.noreply.github.com>
2019-05-24 10:46:52 -0600

    Final authentication app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,230 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import Drawer from "@material-ui/core/Drawer";
+import MenuIcon from "@material-ui/icons/Menu";
+import { Link, Route } from "react-router-dom";
+import { auth } from "./firebase";
+
+export function SignIn(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(u => {
+      if (u) {
+        props.history.push("/app");
+      }
+      // do something
+    });
+
+    return unsubscribe;
+  }, [props.history]);
+
+  const handleSignIn = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {})
+      .catch(error => {
+        alert(error.message);
+      });
+  };
+
+  return (
+    <div>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            Sign In
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Paper style={{ width: "480px", marginTop: "50px", padding: "30px" }}>
+          <TextField
+            placeholder={"Email"}
+            fullWidth={true}
+            value={email}
+            onChange={e => {
+              setEmail(e.target.value);
+            }}
+          />
+          <TextField
+            type={"password"}
+            placeholder="Password"
+            fullWidth={true}
+            style={{ marginTop: "30px" }}
+            value={password}
+            onChange={e => {
+              setPassword(e.target.value);
+            }}
+          />
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginTop: "30px"
+            }}
+          >
+            <Typography>
+              Don't have an account? <Link to="/signup">Sign up!</Link>
+            </Typography>
+            <Button color="primary" variant="contained" onClick={handleSignIn}>
+              Sign in
+            </Button>
+          </div>
+        </Paper>
+      </div>
+    </div>
+  );
+}
+
+export function SignUp(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(u => {
+      if (u) {
+        props.history.push("/app");
+      }
+      // do something
+    });
+
+    return unsubscribe;
+  }, [props.history]);
+
+  const handleSignUp = () => {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {})
+      .catch(error => {
+        alert(error.message);
+      });
+  };
+
+  return (
+    <div>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            Sign Up
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Paper style={{ width: "480px", marginTop: "50px", padding: "30px" }}>
+          <TextField
+            placeholder={"Email"}
+            fullWidth={true}
+            value={email}
+            onChange={e => {
+              setEmail(e.target.value);
+            }}
+          />
+          <TextField
+            type={"password"}
+            placeholder="Password"
+            fullWidth={true}
+            style={{ marginTop: "30px" }}
+            value={password}
+            onChange={e => {
+              setPassword(e.target.value);
+            }}
+          />
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginTop: "30px"
+            }}
+          >
+            <Typography>
+              Already have an account? <Link to="/">Sign in!</Link>
+            </Typography>
+            <Button color="primary" variant="contained" onClick={handleSignUp}>
+              Sign up
+            </Button>
+          </div>
+        </Paper>
+      </div>
+    </div>
+  );
+}
 
 export function App(props) {
-  return <h1 style={{textAlign: 'center'}}>Hello World!</h1>;
+  const [drawer_open, setDrawerOpen] = useState(false);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(u => {
+      if (u) {
+        setUser(u);
+      } else {
+        props.history.push("/");
+      }
+    });
+
+    return unsubscribe;
+  }, [props.history]);
+
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        props.history.push("/");
+      })
+      .catch(error => {
+        alert(error.message);
+      });
+  };
+
+  if (!user) {
+    return <div />;
+  }
+
+  return (
+    <div>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            onClick={() => {
+              setDrawerOpen(true);
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            color="inherit"
+            style={{ flexGrow: 1, marginLeft: "30px" }}
+          >
+            My App
+          </Typography>
+          <Typography color="inherit" style={{ marginRight: "30px" }}>
+            Hi! {user.email}
+          </Typography>
+          <Button color="inherit" onClick={handleSignOut}>
+            Sign out
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        open={drawer_open}
+        onClose={() => {
+          setDrawerOpen(false);
+        }}
+      >
+        <div>Hello</div>
+      </Drawer>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import { App } from "./App";
+import { App, SignIn, SignUp } from "./App";
+import { BrowserRouter, Route } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={SignIn} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/app" component={App} />
+    </div>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
